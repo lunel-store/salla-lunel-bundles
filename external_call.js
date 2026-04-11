b243ec737a7c16c9139f86c8b341a7eb949abf98
Sultan Algarbi <Sultanye8@gmail.com>
2026-04-11 16:09:27 +0300

    7.0.7

--- external_call.js
+++ external_call.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var LUNEL_BUNDLES_CONFIG_VERSION = '7.0.6';
+  var LUNEL_BUNDLES_CONFIG_VERSION = '7.0.7';
   var LUNEL_GITHUB_REPO = 'lunel-store/salla-lunel-bundles';
   var JSDELIVR_PREFIX =
     'https://cdn.jsdelivr.net/gh/' + LUNEL_GITHUB_REPO + '@';
