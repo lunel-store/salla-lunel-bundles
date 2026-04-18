1596d8b750bd059e1c3d5826615bc23a0e5d263d
Sultan Algarbi <Sultanye8@gmail.com>
2026-04-19 02:19:09 +0300

    8.0.4

--- external_call.js
+++ external_call.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const LUNEL_JSDELIVR_LINK =
-    'https://cdn.jsdelivr.net/gh/lunel-store/lunel-bundles@v8.0.3/init.js';
+    'https://cdn.jsdelivr.net/gh/lunel-store/lunel-bundles@v8.0.4/init.js';
 
   if (window.__lunelInitBootstrapExecuted) return;
   window.__lunelInitBootstrapExecuted = true;
