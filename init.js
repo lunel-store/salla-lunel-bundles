5446d278b7248e2994741573cfb4a0a0acaced3a
Sultan Algarbi <Sultanye8@gmail.com>
2026-04-24 15:48:09 +0300

    8.1.5

--- init.js
+++ init.js
@@ -9,7 +9,7 @@
   window.__lunelBundlesInitStarted = true;
 
   window.LUNEL_JSDELIVR_LINK =
-    'https://cdn.jsdelivr.net/gh/lunel-store/lunel-bundles@v8.1.4/';
+    'https://cdn.jsdelivr.net/gh/lunel-store/lunel-bundles@v8.1.5/';
 
   function urlFor(file) {
     const name = typeof file === 'string' ? file.replace(/^\/+/, '') : '';
